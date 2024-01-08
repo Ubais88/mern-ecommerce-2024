53f46880847cf5a9a4863f2db5055a826c432271
Ubais88 <moubais2143@gmail.com>
2024-01-08 10:06:26 +0530

    disount coupon delete controller

--- server/controllers/payment.js
+++ server/controllers/payment.js
@@ -54,3 +54,47 @@ exports.applyDiscount = async (req, res) => {
     });
   }
 };
+
+exports.allCoupons = async (req, res) => {
+  try {
+    const allCoupon = await Coupon.find();
+    res.status(200).json({
+      success: true,
+      data: allCoupon,
+      message: "all coupon fetched successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      error: error.message,
+      message: "Something went wrong",
+    });
+  }
+};
+
+exports.deleteCoupon = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deleteCoupon = await Coupon.findByIdAndDelete(id);
+    if (!deleteCoupon) {
+      return res.status(402).json({
+        success: false,
+        message: "ID is not valid",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: deleteCoupon,
+      message: "coupon deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      error: error.message,
+      message: "Something went wrong",
+    });
+  }
+};
--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { newCoupon, applyDiscount } = require('../controllers/payment');
+const { newCoupon, applyDiscount , allCoupons ,deleteCoupon } = require('../controllers/payment');
 
 
 router.post('/coupon/new', newCoupon)
-router.post('/discount', applyDiscount)
+router.get('/discount', applyDiscount)
+router.get('/coupon/all', allCoupons)
+router.delete('/coupon/:id', deleteCoupon)
 
 module.exports = router;
